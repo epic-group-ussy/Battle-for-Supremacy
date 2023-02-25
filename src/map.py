bf9232f5a896f5be97c86ffc0c34ca1ed34457be
sdp8554 <124285041+sdp8554@users.noreply.github.com>
2023-02-25 16:32:05 -0500

    BUSSY
    ;)

--- src/map.py
+++ src/map.py
@@ -3,12 +3,33 @@ from vector import Vector
 
 class Map:
     def __init__(self):
+        """ALL VECTORS ARE 0,0 AS A PLACEHOLDER
+        PLEASE PLEASE PLEASE DONT FORGET TO CHANGE YOU FUCKING IDIOT KILL YOURSLEF"""
         self.territories = {}
+        #DORMS CONT
+        self.GTD("Gibson","Dorm",Vector(0,0))
+        self.GTD("Sol","Dorm", Vector(0, 0))
+        self.GTD("fish","Dorm",Vector(0, 0))
 
-        self.create_territory(Territory("gibson", Vector(0, 0)))
-        self.create_territory(Territory("ohio", Vector(0, 1)))
-        self.create_territory(Territory("sol", Vector(1, 0)))
-        self.create_territory(Territory("fish", Vector(-1, 0)))
+        #GLOBAL VILLAGE CONT (BLUE)
+
+        #ATTACKING CONT
+        self.GTD("Ohio","Attack",Vector(0,1))
+
+        #FAR CAMPUS (RED)
+
+        #PI QUAD (YELLOW)
+
+        #INFINITY QUAD (ORANGE)
+
+        #SAU THING (TEAL)
+
+        #KODIAK QUAD (PURPLE)
+
+        #GRACIE'S GANG (GREEN)
+
+        #COMMONS LOVERS (YELLOW)
+
 
         return
     def create_territory(self, territory):
@@ -18,7 +39,10 @@ class Map:
             other_territory = self.territories[other_territory_name]
             if(     territory.should_connect(other_territory)  ):
                 territory.add_neighbor(other_territory)
-        
+    def GTD(self,name,continent_name,vecterpos):
+        """Generate Territory Data"""
+        self.create_territory(Territory(name,continent_name,vecterpos))
+
 
 
 
