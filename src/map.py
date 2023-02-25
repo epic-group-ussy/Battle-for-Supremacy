aa5f25deb6c6b0cec02f778dfa9eb4ff7d651831
Sam Maselli <sammypet3@gmail.com>
2023-02-25 16:38:51 -0500

    Update map.py

--- src/map.py
+++ src/map.py
@@ -6,15 +6,9 @@ class Map:
         """ALL VECTORS ARE 0,0 AS A PLACEHOLDER
         PLEASE PLEASE PLEASE DONT FORGET TO CHANGE YOU FUCKING IDIOT KILL YOURSLEF"""
         self.territories = {}
-        #DORMS CONT
-        self.GTD("Gibson","Dorm",Vector(0,0))
-        self.GTD("Sol","Dorm", Vector(0, 0))
-        self.GTD("fish","Dorm",Vector(0, 0))
+
 
         #GLOBAL VILLAGE CONT (BLUE)
-
-        #ATTACKING CONT
-        self.GTD("Ohio","Attack",Vector(0,1))
 
         #FAR CAMPUS (RED)
 
@@ -39,6 +33,8 @@ class Map:
             other_territory = self.territories[other_territory_name]
             if(     territory.should_connect(other_territory)  ):
                 territory.add_neighbor(other_territory)
+
+
     def GTD(self,name,continent_name,vecterpos):
         """Generate Territory Data"""
         self.create_territory(Territory(name,continent_name,vecterpos))
