f5425f5d1bf8c75e471505923794c0326ef6fb3f
Sam Maselli <sammypet3@gmail.com>
2023-02-25 14:02:32 -0500

    territory

---
+++ src/territory.py
@@ -0,0 +1,10 @@
+
+
+class Territory:
+  def __init__(self, name):
+    self.name = name
+    self.team_id = -1
+    self.troop_count = 0
+
+  def __str__(self):
+    return f"{self.name}: Team:{self.team_id}, Count{self.troop_count}"
