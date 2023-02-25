59e5726f98ca6005f074cf61fa1ec88f9b9e09bb
Sam Maselli <sammypet3@gmail.com>
2023-02-25 14:06:28 -0500

    map

---
+++ src/map.py
@@ -0,0 +1,10 @@
+from territory import Territory
+
+class Map:
+    def __init__(self):
+        self.territories = []
+        
+        for i in range(10):
+            self.territories.append(Territory("Sussy"))
+
+        return
--- src/territory.py
+++ src/territory.py
@@ -5,6 +5,7 @@ class Territory:
     self.name = name
     self.team_id = -1
     self.troop_count = 0
+    self.neighbors = []
 
   def __str__(self):
     return f"{self.name}: Team:{self.team_id}, Count{self.troop_count}"
