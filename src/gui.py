d9b3c10b617de1e46822ae340a860e4213b7b776
Sam Maselli <sammypet3@gmail.com>
2023-02-25 15:14:56 -0500

    attack

--- src/gui.py
+++ src/gui.py
@@ -6,7 +6,8 @@ import map
 from kivy.uix.floatlayout import FloatLayout
 class GUImap(App):
     def build(self):
-        
+        screen = Screen()
+
         MAp=map.Map()
         offset = Vector(200, 200)
         #creates a map of buttons from the map object from map.py for each territory
@@ -18,9 +19,9 @@ class GUImap(App):
 
             button = Button(text=territory.name, size_hint=(None, None), size=(100, 100), pos=territory.position.add(offset).to_tuple())
             #button.bind(on_press=self.callback)
+            screen.add_widget(button)
+        return screen
 
-            
-
-        return self.root
+        #return self.root
 
 GUImap().run()
--- src/territory.py
+++ src/territory.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Territory:
   def __init__(self, name, position):
@@ -11,3 +11,24 @@ class Territory:
 
   def __str__(self):
     return f"{self.name}: Team:{self.team_id}, Count{self.troop_count}"
+  
+
+  def attack(self, num_attackers):
+    """returns number to subtract from attackers"""
+    defender_count = max(2, self.troop_count)
+    attacker_count = max(3, num_attackers)
+
+    defender_die = []
+    attacker_die = []
+
+    for _ in range(defender_count):
+      defender_die.append(random.randint(0,6))
+    for _ in range(attacker_count):
+      attacker_die.append(random.randint(0,6))
+
+    defender_die.sort(reverse=True)
+    attacker_die.sort(reverse=True)
+
+    #compare and delete
+    for i in range(min(attacker_count, defender_count)):
+      
