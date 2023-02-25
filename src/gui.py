03e976173e840108ae10c5cba631b15558717d9a
Sam Maselli <sammypet3@gmail.com>
2023-02-25 17:11:55 -0500

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -12,17 +12,20 @@ class GUImap(App):
         screen = Screen()
 
         MAp=map.Map()
-        offset = Vector(0, 0)
+        offset = Vector(25, 25)
         #creates a map of buttons from the map object from map.py for each territory
         #each button will have a label with the territory name
         #each button will have a label with the troop count
         #each button will have a label with the team id
         for territory_name in MAp.territories:
+
             territory = MAp.territories[territory_name]
-            button = Button(text=territory.name, size_hint=(None, None), size=(75, 75), pos=territory.position.multiply(100).add(offset).to_tuple())
+
+            button = Button(text=territory.name, size_hint=(None, None), size=(50, 50), pos=territory.position.multiply(75).to_tuple())
+
             #draws lines between the difrent teritories
             for neighbor in territory.neighbors:
-                button.canvas.add(Line(points=[territory.position.multiply(100).add(offset).to_tuple(), neighbor.position.multiply(100).add(offset).to_tuple()], width=1))
+                button.canvas.add(Line(points=[territory.position.multiply(75).add(offset).to_tuple(), neighbor.position.multiply(75).add(offset).to_tuple()], width=1))
 
             #button.bind(on_press=self.callback)
             screen.add_widget(button)
