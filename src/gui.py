2245a0f108921e76d718b5eac1cd93c980e7aa36
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-02-25 16:45:32 -0500

    lines

--- src/gui.py
+++ src/gui.py
@@ -5,9 +5,10 @@ from kivymd.uix.screen import Screen
 from vector import Vector
 import map
 from kivy.uix.floatlayout import FloatLayout
-
+from kivy.graphics import Line
 class GUImap(App):
     def build(self):
+        """builds the gui"""
         screen = Screen()
 
         MAp=map.Map()
@@ -18,8 +19,11 @@ class GUImap(App):
         #each button will have a label with the team id
         for territory_name in MAp.territories:
             territory = MAp.territories[territory_name]
-
             button = Button(text=territory.name, size_hint=(None, None), size=(75, 75), pos=territory.position.multiply(100).add(offset).to_tuple())
+            #draws lines between the difrent teritories
+            for neighbor in territory.neighbors:
+                button.canvas.add(Line(points=[territory.position.multiply(100).add(offset).to_tuple(), neighbor.position.multiply(100).add(offset).to_tuple()], width=1))
+
             #button.bind(on_press=self.callback)
             screen.add_widget(button)
         return screen
