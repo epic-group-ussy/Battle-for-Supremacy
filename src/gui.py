1780e08da3299b7c4b29efec9b7d4df0d70ae062
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-03-02 07:51:14 -0500

    working new gui
    made the gui work with a .kv file now to remove some of the formating out of gui.py to allow for more readabl;e code

--- src/gui.py
+++ src/gui.py
@@ -1,59 +1,78 @@
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
-from kivymd.uix.screen import Screen
-from vector import Vector
-import map
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Line
-class GUImap(App):
-    def build(self):
-        """builds the gui"""
-        screen = Screen()
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+from kivy.clock import Clock
 
+from vector import Vector
+import map
+
+Builder.load_file('gui.kv')
+
+
+class TerritoryButton(Button):
+    pass
+
+
+class GUIMapScreen(Screen):
+    content_area = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_once(self._build_map)
+
+    def _build_map(self, dt):
         tile_size = 75
-        button_size_multiplier = 2/3
-        #calculated
+        button_size_multiplier = 2 / 3
+        # calculated
         button_size = tile_size * button_size_multiplier
-        center_offset = Vector(button_size/2, button_size/2)
+        center_offset = Vector(button_size / 2, button_size / 2)
 
-        MAp=map.Map()
+        map_obj = map.Map()
 
-        #creates a map of buttons from the map object from map.py for each territory
-        #each button will have a label with the territory name
-        #each button will have a label with the troop count
-        #each button will have a label with the team id
-        #draws lines between the difrent teritories
+        # creates a map of buttons from the map object from map.py for each territory
+        # each button will have a label with the territory name
+        # each button will have a label with the troop count
+        # each button will have a label with the team id
+        # draws lines between the different territories
 
-        #deaws the lines between theritiries 
-        for territory_name in MAp.territories:
-            territory = MAp.territories[territory_name]
+        # draws the lines between territories
+        for territory_name in map_obj.territories:
+            territory = map_obj.territories[territory_name]
             for neighbor in territory.neighbors:
-                screen.canvas.add(Line(points=[
-                    territory.position.multiply(tile_size).add(center_offset).to_tuple(), 
+                self.content_area.canvas.add(Line(points=[
+                    territory.position.multiply(tile_size).add(center_offset).to_tuple(),
                     neighbor.position.multiply(tile_size).add(center_offset).to_tuple()
                 ], width=2))
-        #draws the contenatnes
-        for territory_name in MAp.territories:
 
-            territory = MAp.territories[territory_name]
+        # draws the continents
+        for territory_name in map_obj.territories:
+            territory = map_obj.territories[territory_name]
 
-            button = Button(text=territory.name,
-                            color = (0.25,0.25,0.25,1),
-                            
-                            size_hint=(None, None),
-                            size=(button_size, button_size),
-                            pos=territory.position.multiply(tile_size).to_tuple(),
-                            background_color=MAp.colors[territory.continent_name],
-                            background_normal=''
-                        )
+            button = TerritoryButton(text=territory.name,
+                color=(0.25, 0.25, 0.25, 1),
+                size_hint=(None, None),
+                size=(button_size, button_size),
+                pos=territory.position.multiply(tile_size).to_tuple(),
+                background_color=map_obj.colors[territory.continent_name],
+                background_normal=''
+                )
 
-            #button.bind(on_press=self.callback)
-            screen.add_widget(button)
-        
-        
-        
-        
-        return screen
+            self.content_area.add_widget(button)
 
-GUImap().run()
+
+class GUIMapApp(App):
+    def build(self):
+        # create screen manager and add GUIMapScreen
+        screen_manager = ScreenManager()
+        guimap_screen = GUIMapScreen(name='map_screen')
+        screen_manager.add_widget(guimap_screen)
+        return screen_manager
+
+
+if __name__ == '__main__':
+    GUIMapApp().run()
